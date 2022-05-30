1b87235d0dbab55e7b31d40f2833e4d662469f79
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-05-30 22:34:19 +0200

    Tested user/register endpoint

---
+++ src/db/routers/usersRouter.test.js
@@ -0,0 +1,37 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { mongoose } = require("mongoose");
+const request = require("supertest");
+const connectDB = require("..");
+const app = require("../../server");
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+
+  await connectDB(mongoServer.getUri());
+});
+afterAll(async () => {
+  await mongoServer.stop();
+  await mongoose.connection.close();
+});
+
+describe("Given a POST /user/register endpoint", () => {
+  describe("When it receives a request with a new user", () => {
+    test("Then it should call the res json method with the username ", async () => {
+      const newUser = {
+        name: "Morgan",
+        username: "morganfreeman",
+        password: "morgan",
+        email: "morgan@freeman.com",
+      };
+
+      const { body } = await request(app)
+        .post("/user/register")
+        .send(newUser)
+        .expect(201);
+
+      expect(body).toBe(newUser.username);
+    });
+  });
+});
