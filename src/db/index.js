9354a4408f9d549fd674fd8649a5f43874a02a24
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-06-01 10:40:17 +0200

    Modified the json returned object from the database

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,19 @@ const mongoose = require("mongoose");
 const connectDB = (connectionString) =>
   new Promise((resolve, reject) => {
     mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        const newReturnedJSON = { ...ret };
+
+        // eslint-disable-next-line no-underscore-dangle
+        delete newReturnedJSON._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete newReturnedJSON.__v;
+
+        return newReturnedJSON;
+      },
+    });
     mongoose.connect(connectionString, (error) => {
       if (error) {
         debug(chalk.red("Error on connecting to database:", error.message));
